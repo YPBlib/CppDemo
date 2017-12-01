3fcdd53e735f30cd6c891abaa6e76461aa307e68
YPBlib <ypb1024@foxmail.com>
2017-12-02 05:38:52 +0800

    l

--- draft.cpp
+++ draft.cpp
@@ -18,6 +18,7 @@
 #include<memory>
 #include<new>
 #include<utility>
+#include<functional>
 #include"try.h"
 //using namespace::std;
 using std::vector;
@@ -447,11 +448,66 @@ vector<int>::const_iterator iveccoit;
 	int i3 = 5;
 	int&& rr = std::move(i3);
 */
-
+	
 
 	return 0;
 }
+template <typename T>
+int compare(const T& b1, const T& v2)
+{
+	if (v1 < v2) return -1;
+	if (v2 < v1) return 1;
+	return 0;
+}
 
+template <typename T>
+class Blob
+{
+public:
+	typedef T value_type;
+	typedef typename std::vector<T>::size_type size_type;
+	//constructors
+	Blob();
+	Blob(std::initializer_list<T> i1);
+
+	size_type size() const { return data->size(); }
+	bool empty() const { return data->empty(); }
+
+	void push_back(const T& t) { data->push_back(t); }
+	void push_back(T &&t) { data->push_back(std::move(t)); }
+	void pop_back();
+
+	T& back();
+	T& operator[](size_type i);
+private:
+	std::shared_ptr<vector<T>> data;
+	void check(size_type i, const string &msg) const;
+};
+template <typename T>
+void Blob<T>::check(size_type i, const string &msg) const
+{
+	if (i >= data->size())
+	{
+		throw std::out_of_range(msg);
+	}
+}
+template <typename T>
+T& Blob<T>::back() 
+{
+	check(0, "back on empty Blob");
+	return data->back();
+}
+template <typename T>
+T& Blob<T>::operator[](size_type i)
+{
+	check(i, "sunscript out of range");
+	return (*data)[i];
+}
+template <typename T>
+Blob<T>::Blob():data(std::make_shared<vector<T>>()){}
+template <typename T>
+Blob<T>::Blob(std::initializer_list<T> i1) : 
+	data(std::make_shared<vector<T>>(i1)) {}
 
 int mySpDeleter(std::shared_ptr<int> sp0)
 {
