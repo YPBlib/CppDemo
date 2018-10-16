b0fa3898fbe37848c25769157bc9adb5f42409a3
YPBlib <ypb1024@foxmail.com>
2018-10-16 23:48:51 +0800

    edit async phase 1

--- concurrency.cpp
+++ concurrency.cpp
@@ -1,99 +1,11 @@
-#include <iostream>
-#include <future>
-#include <random>
-#include <thread>
-#include <chrono>
-#include <exception>
-#include <atomic>
-#include <mutex>
-#include <condition_variable>
-
-using std::cout;
-using std::endl;
-using std::cin;
-using std::cerr;
-using std::exception;
-using std::runtime_error;
+#include "concurrency.h"
+#include "async.h"
 
 
 
-int doSomething(char c)
-{
-	std::default_random_engine dre(c);
-	std::uniform_int_distribution<int> id(10, 1000);
-	for (int i = 0; i < 10; ++i)
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(id(dre)));
-		std::cout.put(c).flush();
-	}
-	return c;
-}
 
-int foo1()
-{
-	return doSomething('*');
-//		_AddressOfReturnAddress();
-}
 
-int foo2()
-{
-	return doSomething('+');
-}
 
-int test_async()
-{
-		
-	cout << __FUNCTION__ << endl;
-	std::cout << "starting foo1() in background, foo2() in foreground" << std::endl;
-
-	//std::future<int> result1(std::async(foo1));
-	std::future<int> result1(std::async(std::launch::async, foo1));
-	//std::future<int> result1(std::async(std::launch::deferred,foo1));
-	int result = 0;
-
-	// version 1 :async
-	// call early and return late
-		int result2 = foo2();
-		result = result1.get() + result2;
-
-	// version 2 : may cause sequential exec
-	// result =result1.get()+ foo2();
-	// result = foo2() + result1.get();
-	std::cout << "\nfoo1() + foo2(): " << result << std::endl;
-	return 0;
-}
-
-static int i = 0;
-int f_throw()
-{
-		
-	for (; i < 10;)
-	{
-		++i;
-		f_utils();
-		if (i > 6)throw runtime_error("excep!\n");
-	}
-	return i;
-}
-void f_utils()
-{
-	cout << "f_utils:"<<i << endl;
-}
-
-void test_async_excep()
-{
-	cout << __FUNCTION__ << endl;
-	auto f1 = std::async(std::launch::async,f_throw);
-	f_utils();
-	try
-	{
-		f1.get();
-	}
-	catch(const exception& e)
-	{
-		cerr << e.what() << endl;
-	}
-}
 
 struct X
 {
@@ -116,6 +28,7 @@ void test_promise()
 	cout << std::this_thread::get_id() << endl;
 
 }
+
 
 
 int main()
---
+++ concurrency.h
@@ -0,0 +1,23 @@
+#ifndef ZJUSCT2019
+#define ZJUSCT2019
+
+#include <iostream>
+#include <future>
+#include <random>
+#include <thread>
+#include <chrono>
+#include <exception>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::cerr;
+using std::exception;
+using std::runtime_error;
+
+#endif // !ZJUSCT2019
+
+
